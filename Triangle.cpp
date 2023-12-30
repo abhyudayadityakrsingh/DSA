087d13e4ea05a4e40769eeaefbae65dc3eedb2fc
Abhyudayaditya Kumar Singh <92624016+abhyudayadityakrsingh@users.noreply.github.com>
2023-12-30 11:51:26 +0530

    Update Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -32,3 +32,33 @@ public:
        return dp[0][0] ;
     }
 };
+
+
+
+// Space Optimization Technique - Highly efficient
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size() ;
+       vector <int> front(n) ;
+        
+        //compute base case -> last row
+        for(int i = 0 ; i < n; i ++) {
+            front[i] = triangle[n-1][i];
+        }
+
+        for(int i = n-2 ; i >= 0 ; i -- ){
+           vector <int> curr(n) ;
+            for(int j = i ; j>= 0 ; j-- ){
+                int d = triangle[i][j] + front[j] ;
+                int dg = triangle[i][j] + front[j+1] ;
+                curr[j] = min(d,dg) ;
+
+            }
+            front = curr ;
+        }
+
+        return front[0] ;
+    }
+};
