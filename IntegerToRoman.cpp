405cb91415b9d40e540699559079b9ff202810c5
Abhyudayaditya Kumar Singh <92624016+abhyudayadityakrsingh@users.noreply.github.com>
2024-01-13 22:28:20 +0530

    Create IntegerToRoman.cpp

---
+++ IntegerToRoman.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string s="";
+       while(num>=1000){
+          s=s+'M';
+          num=num-1000;
+       }
+       if(num>=900){
+          s=s+"CM";
+          num=num-900;
+       }
+       if(num>=500){
+          s=s+'D';
+          num=num-500;
+       }
+       if(num>=400){
+          s=s+"CD";
+          num=num-400;
+       }
+       while(num>=100){
+          s=s+'C';
+          num=num-100;
+       }
+       if(num>=90){
+          s=s+"XC";
+          num=num-90;
+       }
+       if(num>=50){
+          s=s+'L';
+          num=num-50;
+       }
+       if(num>=40){
+          s=s+"XL";
+          num=num-40;
+       }
+      while(num>=10){
+          s=s+'X';
+          num=num-10;
+       }
+       if(num>=9){
+          s=s+"IX";
+          num=num-9;
+       }
+       if(num>=5){
+          s=s+'V';
+          num=num-5;
+       }
+       if(num>=4){
+          s=s+"IV";
+          num=num-4;
+       }
+       while(num--){
+          s=s+"I";
+       }
+    return s;
+    }
+};
