17d8b1f5b6aa9d1b97b15238ded8e104ec33fb76
Abhyudayaditya Kumar Singh <92624016+abhyudayadityakrsingh@users.noreply.github.com>
2024-01-26 15:05:05 +0530

    Create MatrixChainMultpilication.cpp

---
+++ MatrixChainMultpilication.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+int f(int i , int j , int* arr,vector<vector<int>>&dp){
+    if(i == j ) return  0 ;
+    if(dp[i][j] != -1) return dp[i][j] ;
+    int mini = 1e9 ;
+    for(int k=i ; k<j; k++){
+         int steps= arr[i-1] * arr[k] * arr[j] + f(i,k,arr,dp) + f(k+1,j,arr,dp) ;
+         if(steps < mini) 
+            mini = steps;
+    }
+    return dp[i][j] = mini ;
+}
+int matrixChainMultiplication(int* arr, int n) {
+    // Write your code here
+    vector <vector <int>> dp(n , vector<int>(n,-1));
+    return f(1,n-1,arr,dp) ;
+}
