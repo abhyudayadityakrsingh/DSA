b8f12c545641090e091681aad1c509b51053c087
Abhyudayaditya Kumar Singh <92624016+abhyudayadityakrsingh@users.noreply.github.com>
2024-01-26 15:40:29 +0530

    Update MatrixChainMultpilication.cpp

--- MatrixChainMultpilication.cpp
+++ MatrixChainMultpilication.cpp
@@ -12,6 +12,18 @@ int f(int i , int j , int* arr,vector<vector<int>>&dp){
 }
 int matrixChainMultiplication(int* arr, int n) {
     // Write your code here
-    vector <vector <int>> dp(n , vector<int>(n,-1));
-    return f(1,n-1,arr,dp) ;
+    vector <vector <int>> dp(n , vector<int>(n,0));
+    for(int i=0; i<n;i++) dp[i][i] =0 ;
+
+    for(int i=n-1;i>=0;i--){
+        for(int j=i+1;j<n;j++){
+            int mini = 1e9 ;
+            for(int k=i ; k<j; k++){
+                int steps = arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j] ;
+                if(steps <mini) mini = steps;
+            }
+            dp[i][j] = mini;
+        }
+    }
+    return dp[1][n-1] ;
 }
